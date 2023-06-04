882a3784b6580e2a58edce4325ce4d9496e7a1ec
Tsvetislav <tsvetislav.todorov@mentormate.com>
2023-06-04 22:20:51 +0300

    Use both HTTP and WS

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
 const port = process.env.PORT || 3030;
 
-const wss = new WebSocketServer({ port });
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, HTTP!');
+});
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(message) {
-    const data = JSON.parse(message);
+// Create a WebSocket server by attaching it to the HTTP server
+const wss = new WebSocket.Server({ server });
 
-    if (data.type === 'message') {
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: 'message', data: data.data }));
-        }
-      });
-    }
+// WebSocket connection handling
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+    ws.send('Server received your message: ' + message);
   });
+
+  ws.on('close', () => {
+    console.log('WebSocket connection closed');
+  });
+
+  ws.send('Welcome to the WebSocket server!');
+});
+
+// Start the HTTP server
+server.listen(port, () => {
+  console.log('HTTP server listening on ' + port);
 });
