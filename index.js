a5bcdcda52be2012da0ccdc1b3dc593a0fc4c600
Tsvetislav <tsvetislav.todorov@mentormate.com>
2023-06-04 14:23:11 +0300

    Use HTTP and upgrade to WS on connection

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
 const port = process.env.PORT || 3030;
 
-const wss = new WebSocketServer({ port });
+const wss = new WebSocketServer({ noServer: true });
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World!');
+    res.end();
+
+    req.on('upgrade', (req, socket, head) => {
+      wss.handleUpgrade(req, socket, head, (ws) => {
+        wss.emit('connection', ws, req);
+      });
+    });
+  })
+  .listen(port);
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(message) {
